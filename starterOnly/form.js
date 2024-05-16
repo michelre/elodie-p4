ce58afa34862c1f77391045a18b2530b2eaab36e
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2024-05-16 10:41:43 +0200

    gestion des modals

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,22 +1,33 @@
 const form = document.querySelector('form')
 
 const validateName = (field) => {
-  
+   const regexName = /^([A-Za-z|\s]{2,30})?([-]{0,1})?([A-Za-z|\s]{2,30})$/;
+   return regexName.test(field.value);
 }
 
 
 const validateEmail = (field) => {
-  
+    const regexEmail = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/
+    return regexEmail.test(field.value);
 }
 
 
 const validateBirthdate = (field) => {
-
+    // Récupérer la date de naissance 
+    const birthdate = new Date(field.value);
+    
+    // Créer une date il y a 18 ans à partir de la date actuelle
+    const eighteenYearsAgo = new Date();
+    eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+    
+    // Comparer la date de naissance avec il y a 18 ans
+    return birthdate <= eighteenYearsAgo;
 }
 
 
 const validateQuantity = (field) => {
-   
+    const regexQuantity = /^([0-9]{1,2})$/;
+   return regexQuantity.test(field.value);
 }
 
 const validateLocation = () => {
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,11 +11,15 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const ModalSuccess = document.querySelector('#modal-success');
+const closeSuccess = document.querySelector('#closeSuccess');
+const closeForm = document.querySelector('#closeForm');
+const btnCloseSucces = document.querySelector('#btn-close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// ouverture de la modal formulaire
 function launchModal() {
   modalbg.style.display = "block";
 }
@@ -24,4 +28,26 @@ function hideModal() {
   modalbg.style.display = "none";
 }
 
+function successModal() {
+  ModalSuccess.style.display = "block"; 
+}
 
+//fermeture de la modale formulaire
+closeForm.addEventListener("click", closeModalForm);
+
+function closeModalForm() {
+  modalbg.style.display = "none";
+}
+
+//fermeture de la modale de succes
+closeSuccess.addEventListener("click", closeModalSuccess);
+
+function closeModalSuccess() {
+  ModalSuccess.style.display = "none";
+}
+//fermeture de la modale avec le btn fermer
+btnCloseSucces.addEventListener("click", closeModalSuccess);
+
+function closeModalSuccess() {
+  ModalSuccess.style.display = "none";
+}
